f15b11ffdabcd4055c7148276dece399c414ad66
wael <wsebaiy@gmail.com>
2022-03-09 22:27:16 +0100

    Forming random message

--- script.js
+++ script.js
@@ -12,17 +12,21 @@ const arr1 = [
     "Sagittarius",
     "Capricorn",
 ];
-const arr2 = ["good luck", "Bad luck", "Fair luck"];
+const arr2 = ["good luck", "Bad luck", "Fair luck", "depression", "motivation"];
 const arr3 = [
     "trust no one",
     "Work hard",
     "alwayes have hope",
     "invest in yourself",
     "get more friends",
-    "do not give up",
+    "not give up",
 ];
 let first = arr1[Math.floor(Math.random() * arr1.length)];
 
 let second = arr2[Math.floor(Math.random() * arr2.length)];
 let third = arr3[Math.floor(Math.random() * arr3.length)];
-console.log(first, second, third);
+
+let message = `Your Sign is ${first}.
+Your are having ${second}.
+You must ${third}.`;
+console.log(message);
