f38ae06520e4ee398afc5de77792c0d3a9db1f4f
wael <wsebaiy@gmail.com>
2022-03-09 21:49:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const arr1 = [
+    "Aquarius",
+    "Pisces",
+    "Aries",
+    "Turus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "virgo",
+    "Lipra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+];
+const arr2 = ["good luck", "Bad luck", "Fair luck"];
+const arr3 = [
+    "trust no one",
+    "Work hard",
+    "alwayes have hope",
+    "invest in yourself",
+    "get more friends",
+    "do not give up",
+];
